7fefe20dd5a5e335c49c22c7b9cdd646afa8febf
P S Pooja Sree <pspoojasree11@gmail.com>
2026-08-02 19:07:42 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,23 @@
+"""
+print("Hello Pooja !")
+print(" Good \n morning :) ")
+phrase="how are you"
+print(phrase +" guys "+phrase +" guys "+phrase)
+print(phrase.upper())
+"""
+
+#Strings - Functions - UPEER CASE & LOWER CASE 
+"""
+phrase = " Pooja Sree "
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.islower())
+print(phrase.lower().islower())
+print(phrase.upper().isupper())
+"""
+
+#Strings - Functions - LENGTH
+phrase = "Pooja Sree"
+print(len(phrase))
+print(phrase[0:5])
